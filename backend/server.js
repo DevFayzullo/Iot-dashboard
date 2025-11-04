ca343bae7d4eb1b6a836198f5dab5ef8fdbb36b2
DevFayzullo <fayzullo.coder@gmail.com>
2025-11-04 11:50:16 +0900

    something chenged. but there is a database issuee....

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+require("dotenv").config();
+console.log("DB_URI=", process.env.DB_URI);
 const apiRoutes = require("./routes/apiRoutes");
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
